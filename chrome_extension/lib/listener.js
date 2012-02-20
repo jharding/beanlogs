309e6edacf287ceb5d393400778c8861d5c09f61
Jake Harding <jacob.s.harding@gmail.com>
2012-02-20 01:14:28 -0800

    Updating extension.

--- chrome_extension/lib/listener.js
+++ chrome_extension/lib/listener.js
@@ -1,15 +1,31 @@
-chrome.history.onVisited.addListener(function(result) {
-    var url = 'http://localhost:3000/log';
-    var data = {
-        url: result.url,
-        timestamp: result.lastVisitTime
-    };
-    $.ajax({
-        url: url,
-        type: 'post',
-        data: data,
-        xhrFields: {
-            withCredentials: true
-        }
+(function() {
+    var host = 'http://localhost:3000';
+
+    chrome.history.onVisited.addListener(function(result) {
+        var url = host + '/api/log';
+        $.ajax({
+            url: url,
+            type: 'post',
+            dataType: 'json',
+            xhrFields: {
+                withCredentials: true
+            },
+            data: {
+                url: result.url,
+                timestamp: result.lastVisitTime
+            },
+            error: function() {
+                chrome.browserAction.setIcon({
+                    path: '../assets/icon1.png'
+                });
+            },
+            success: function(data) {
+                if (!data.success) {
+                    chrome.browserAction.setIcon({
+                        path: '../assets/icon2.png'
+                    });
+                }
+            }
+        });
     });
-});
+})()
